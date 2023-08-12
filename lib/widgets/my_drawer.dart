329cc460e0d58ced8c6b0b86b457967997f6f81e
Provu Das <pro119449@gmail.com>
2023-08-12 21:17:22 +0530

    offer 500

--- lib/widgets/my_drawer.dart
+++ lib/widgets/my_drawer.dart
@@ -6,6 +6,7 @@ import 'package:provider/provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import '../global/global.dart';
 
+//MyDrawer
 class MyDrawer extends StatefulWidget {
   String? name;
   String? phone;
